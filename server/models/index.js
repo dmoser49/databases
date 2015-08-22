26013c6629265427f963da7dd0d7d1bef5ffd45d
MKS-students <mks.students@hackreactor.com>
2015-08-21 19:46:11 -0500

    first commit

--- server/models/index.js
+++ server/models/index.js
@@ -5,14 +5,30 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (err, cb) {
+      db.dbConnection.query('use ' + chat);
+      db.dbConnection.query('select * from messages', function(err){
+        if (err){
+          throw err;
+        }
+      })
+      // request.on('end', function(){
+      //   connection.query()
+      })
+    }, // a function which produces all the messages
+    post: function (err, cb) {
+
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function () {
+
+    },
+    post: function () {
+
+    }
   }
 };
 
--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,77 @@
-CREATE DATABASE chat;
-
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'users'
+--
+-- ---
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  username VARCHAR(250) NULL DEFAULT NULL,
+  password VARCHAR(250) NULL DEFAULT NULL,
+  PRIMARY KEY (id)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'messages'
+--
+-- ---
 
+DROP TABLE IF EXISTS messages;
 
+CREATE TABLE messages (
+  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  text VARCHAR(250) NULL DEFAULT NULL,
+  sender INTEGER NULL DEFAULT NULL,
+  room_id INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+);
 
+-- ---
+-- Table 'rooms'
+--
+-- ---
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+DROP TABLE IF EXISTS rooms;
 
+CREATE TABLE rooms (
+  id INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  roomname VARCHAR(250) NULL DEFAULT NULL,
+  PRIMARY KEY (id)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE messages ADD FOREIGN KEY (room_id) REFERENCES rooms (id);
+ALTER TABLE messages ADD FOREIGN KEY (sender) REFERENCES users (id);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE users ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE messages ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE rooms ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO users (id,username,password) VALUES
+-- ('','','');
+-- INSERT INTO messages (id,text,sender) VALUES
+-- ('','','');
+-- INSERT INTO rooms (id,roomname) VALUES
+-- ('','');
